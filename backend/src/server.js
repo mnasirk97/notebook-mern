ebd104813226d6dc48fdd74ff3fe7f87f9ff0db0
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2025-06-15 22:32:28 +0500

    Add server.js to set up Express server with environment variables and routes

---
+++ backend/src/server.js
@@ -0,0 +1,33 @@
+// const express = require('express'); // common js
+import express from 'express';  // module js
+import dotenv from 'dotenv'; // Importing dotenv to manage environment variables
+
+import notesRoutes from './routes/notesRoutes.js'; // Importing the notes routes
+import { connectDB } from './config/db.js';
+import rateLimiter from './middlewares/rateLimiter.js';
+
+dotenv.config(); // Load environment variables from .env file
+ 
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// middleware
+app.use(express.json());
+app.use(rateLimiter)
+
+
+// ==> test middleware
+// app.use((req, res, next) => {
+//   console.log(`${req.method} request for '${req.url}'`);
+//   next(); // Call the next middleware or route handler
+// })
+
+app.use('/api/notes', notesRoutes); // Mounting the notes routes
+
+
+connectDB().then(()=> {
+  app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
+});
