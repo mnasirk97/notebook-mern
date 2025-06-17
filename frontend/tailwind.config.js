dea3f44539a5173f0fa3d21a9c7853ae09900200
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2025-06-18 00:00:41 +0500

    Add DaisyUI plugin and configure themes in Tailwind CSS

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,3 +1,5 @@
+import daisyui from 'daisyui';
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -7,5 +9,9 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
+  daisyui: {
+    themes: ["forest"]
+    // themes: ["light", "dark", "cupcake", "forest", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "lofi", "dracula", "cmyk", "business", "acid", "lemonade", "night"],
+  },
 }
