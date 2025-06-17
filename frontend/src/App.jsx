d9f8ef0b20a446a54169bda758548fffc82da4cb
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2025-06-17 23:27:46 +0500

    Implement routing and toast notifications in App component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,25 @@
 import React from 'react'
+import { Route, Routes } from 'react-router'
+import { toast } from 'react-hot-toast'
 
-const App = () => {
+import HomePage from './pages/HomePage'
+import CreatePage from './pages/CreatePage'
+import NoteDetailPage from './pages/NoteDetailPage'
+function App() {
   return (
-    <div>
-      App
+    <div data-theme="forest">
+        <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+      <button onClick={()=> toast.success("Congrates")}>click me</button>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NoteDetailPage />} />
+      </Routes>
+      <button onClick={()=> toast.error("Something went wrong")}>click me</button>
     </div>
   )
 }
 
 export default App
-// 1:48
