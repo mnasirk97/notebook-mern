0a8cb681b8d361186e5a8185308c3cf25318f2c8
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2025-06-19 22:31:58 +0500

    removed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <Route path="/note/:id" element={<NoteDetailPage />} />
     </Routes>
 
-    {/* 2:03 */}
+   
 
       {/* Example of tailwind */}
         {/* <h1 className="text-3xl font-bold underline">Hello world!</h1> */}
