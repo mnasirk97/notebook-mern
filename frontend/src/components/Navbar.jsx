6ce14abf154bca7d8ccd153ae30af8c183bc9147
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2025-06-20 22:37:06 +0500

    add style

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ function Navbar() {
     <header className='bg-base-300 border-b border-base-content/10 '>
       <div className='mx-auto max-w-6xl p-4'>
         <div className='flex items-center justify-between'>
-            <h1 >
+            <h1 className='text-3xl font-bold'>
                 NasirPaid
             </h1>
         </div>
