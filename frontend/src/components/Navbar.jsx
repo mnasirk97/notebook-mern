9117c706233a1926c0ad44fbe9cb4b9f5ddabd97
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2025-06-20 22:31:30 +0500

    add navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 
 function Navbar() {
   return (
-    <div>
-      
-    </div>
+    <header className='bg-base-300 border-b border-base-content/10 '>
+      <div>
+        
+      </div>
+    </header>
   )
 }
 
