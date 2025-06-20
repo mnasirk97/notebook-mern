90bb0fc4e9a91726a1adbf44bcf54bb8eeeccb7a
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2025-06-20 22:36:53 +0500

    add logo

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -5,7 +5,9 @@ function Navbar() {
     <header className='bg-base-300 border-b border-base-content/10 '>
       <div className='mx-auto max-w-6xl p-4'>
         <div className='flex items-center justify-between'>
-            
+            <h1 >
+                NasirPaid
+            </h1>
         </div>
       </div>
     </header>
