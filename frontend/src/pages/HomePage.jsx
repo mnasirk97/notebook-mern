4576975ddf08baf6a942abcb3790fedcda96ef4f
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2025-06-24 00:20:50 +0500

    Refactor HomePage component to include note fetching logic and remove unused code

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Navbar from '../components/Navbar'
 import RateLimitedUI from '../components/RateLimitedUI'
 
@@ -6,7 +6,46 @@ function HomePage() {
   const [isRateLimited, setIsRateLimited] = useState(true)
   const [notes, setNotes] = useState([])
   const [loading, setLoading] = useState(true)
-  
+
+  useEffect(() => {
+    const fetchNotes = async () => {
+      try {
+        const res = await fetch('http://localhost:5000/api/notes')
+        const data = await res.json()
+        console.log("Date =>", data)
+      } catch (error) {
+        console.error('Failed to fetch notes:', error)
+      }
+    }
+  }, [])
+
+  // ===> Testing
+  // useEffect(() => {
+  //   const fetchNotes = async () => {
+  //     try {
+  //       const response = await fetch('/api/notes')
+  //       if (!response.ok) {
+  //         throw new Error('Network response was not ok')
+  //       }
+  //       const data = await response.json()
+  //       setNotes(data)
+  //     } catch (error) {
+  //       console.error('Failed to fetch notes:', error)
+  //     } finally {
+  //       setLoading(false)
+  //     }
+  //   }
+
+  //   fetchNotes()
+
+  //   // Simulate rate limit for demonstration purposes
+  //   const rateLimitTimeout = setTimeout(() => {
+  //     setIsRateLimited(false)
+  //   }, 5000) // 5 seconds
+
+  //   return () => clearTimeout(rateLimitTimeout)
+  // }, [])
+
   return (
     <div className='min-h-screen'>
       <Navbar />
