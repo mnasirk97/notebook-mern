157b6c8684b90497400f1b043b05eab45335872d
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2025-06-24 00:01:22 +0500

    Add RateLimitedUI component to HomePage for rate limiting feedback

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,11 +1,14 @@
 import { useState } from 'react'
 import Navbar from '../components/Navbar'
+import RateLimitedUI from '../components/RateLimitedUI'
 
 function HomePage() {
   const [isRateLimited, setIsRateLimited] = useState(false)
   return (
     <div className='min-h-screen'>
       <Navbar />
+
+      {isRateLimited && <RateLimitedUI />}
     </div>
   )
 }
