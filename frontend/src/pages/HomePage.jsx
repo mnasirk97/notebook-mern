a2c4056fe3c809aab49de7d23836168e3a49f320
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2025-06-28 00:53:52 +0500

    refactor: change loading state initialization to false in HomePage component

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -7,7 +7,7 @@ import  toast  from 'react-hot-toast'
 function HomePage() {
   const [isRateLimited, setIsRateLimited] = useState(false)
   const [notes, setNotes] = useState([])
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
     const fetchNotes = async () => {
@@ -68,6 +68,26 @@ function HomePage() {
       <Navbar />
 
       {isRateLimited && <RateLimitedUI />}
+      {console.log("notes", notes)}
+      {notes.map((note) =>
+      {
+        console.log("note", note)
+      })
+
+        }
+      <div className="max-w-7xl mx-auto p-4 mt-6">
+        {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
+            {notes.length > 0 && !isRateLimited && (
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+            {notes.map((note) => (
+             <div>
+              {console.log("note", note)}
+              {note.title} | {note.content}
+             </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   )
 }
