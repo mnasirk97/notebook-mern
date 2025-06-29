9fddfeb95859f68d6c6144513b7eca220ba16e91
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2025-06-30 02:03:07 +0500

    Refactor HomePage component: integrate NoteCard for rendering notes and clean up commented code

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar'
 import RateLimitedUI from '../components/RateLimitedUI'
 import axios from 'axios'
 import  toast  from 'react-hot-toast'
+import NoteCard from '../components/NoteCard'
 
 function HomePage() {
   const [isRateLimited, setIsRateLimited] = useState(false)
@@ -94,22 +95,24 @@ function HomePage() {
 
       {isRateLimited && <RateLimitedUI />}
       {console.log("notes in return", notes)}
-      {/* <div className="max-w-7xl mx-auto p-4 mt-6">
+      <div className="max-w-7xl mx-auto p-4 mt-6">
         {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
 
             {notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {notes.map((note) => (
-             <div key={note.id}>
-                  {note.title} | {note.content}
-              </div>
+              <NoteCard key={note._id} note={note} />
+            //  <div key={note.id}>
+            //       {note.title} | {note.content}
+            //   </div>
             ))}
           </div>
         )}
-      </div> */}
+      </div>
 
 
-      <div className="max-w-7xl mx-auto p-4 mt-6">
+      {/* For debugging */}
+      {/* <div className="max-w-7xl mx-auto p-4 mt-6">
   {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
 
   {!loading && notes.length === 0 && !isRateLimited && (
@@ -125,7 +128,7 @@ function HomePage() {
       ))}
     </div>
   )}
-</div>
+      </div> */}
 
     </div>
   )
