173735c5b201ccc4ead5a63ce786c242b62253a3
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2025-06-30 01:42:31 +0500

    Refactor HomePage component: clean up console logs and commented code

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -69,15 +69,21 @@ function HomePage() {
 
       {isRateLimited && <RateLimitedUI />}
       {console.log("notes", notes)}
-      {notes.map((note) =>
-      {
-        console.log("note", note)
-      })
+      {/* {console.log("notes Data", notes.data)} */}
+      
+      {console.log("Notes Data", data)}
 
-        }
+      
+      {/* {console.log("Notes Content", notes.data.content)} */}
+      {/* {notes.map((note) =>{
+        console.log("note", note)
+      }
+    )
+
+        } */}
       <div className="max-w-7xl mx-auto p-4 mt-6">
         {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
-        
+
             {notes.length > 0 && !isRateLimited && (
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {notes.map((note) => (
