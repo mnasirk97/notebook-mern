f77f8d6909ebd2c2ab3cb56226d72db638e63ff9
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2025-07-02 23:55:52 +0500

    refactor: update NoteCard component to accept note prop and render link

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { Link } from 'react-router'
 
-function NoteCard() {
+function NoteCard({ note }) {
   return (
-    <div>
-      NoteCard
-    </div>
+    <Link to={`/note/${note._id}`} className="note-card">
+      {note.title}
+    </Link>
   )
 }
 
