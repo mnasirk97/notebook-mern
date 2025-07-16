fc05b8d9b2427df11113e7479cd0d1fbe5755141
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2025-07-17 00:53:05 +0500

    Add state

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -2,6 +2,7 @@ import { useState } from "react"
 
 function CreatePage() {
   const [title, setTitle] = useState('')
+  const [content, setContent] = useState('')
   return (
     <div>
       CreatePage
