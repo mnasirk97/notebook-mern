5415821c9a324233901f2acfc60479d3eb48656c
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2025-07-17 00:55:07 +0500

    create an empty function

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -4,6 +4,10 @@ function CreatePage() {
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
   const [loading, setLoading] = useState(false)
+
+  const handleSubmit = () => {
+
+  }
   return (
     <div>
       CreatePage
