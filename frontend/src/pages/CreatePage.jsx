5b6cabe4eddf0a3fccd87e1639b923dabf43f3a2
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2025-07-17 00:53:58 +0500

    Add state for loading

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -3,6 +3,7 @@ import { useState } from "react"
 function CreatePage() {
   const [title, setTitle] = useState('')
   const [content, setContent] = useState('')
+  const [loading, setLoading] = useState(false)
   return (
     <div>
       CreatePage
