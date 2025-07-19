171162980e31ff986f07dfacdcafef0b0945e152
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2025-07-19 22:44:31 +0500

    Add styling

--- frontend/src/pages/CreatePage.jsx
+++ frontend/src/pages/CreatePage.jsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import { Link } from "react-dom";
+import { ArrowLeftIcon } from "lucide-react";
+
 
 function CreatePage() {
   const [title, setTitle] = useState("");
@@ -9,8 +12,11 @@ function CreatePage() {
   return (
     <div className="min-h-screen bg-base-200">
       <div className="container mx-auto px-4 py-8">
-        <div>
-          
+        <div className="max-w-2xl mx-auto">
+          <Link to={"/"} className="btn btn-ghost mb-6">
+          <ArrowLeftIcon className="size-5" />
+            Back to Notes
+          </Link>
         </div>
       </div>
     </div>
