1afb2ae4e9f75ca15724f36c21f1abd689c580ee
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2025-07-22 23:57:02 +0500

    removed commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-// import { Route, Routes } from 'react-router'
+// import { Route, Routes } from 
 import { Route, Routes } from 'react-router-dom'
 import { toast } from 'react-hot-toast'
 
