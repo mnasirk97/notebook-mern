936207eebab238bcb24cfbc32ccec8ac214ff872
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2025-07-28 00:06:06 +0500

    Add time

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -6,7 +6,7 @@ import { formatDate } from '../lib/utils'
 function NoteCard({ note }) {
 
   const handleDelete = () => {
-    {/* 2:50:20 */}
+    {/* 2:50:00 */}
   }
   return (
     <Link to={`/note/${note._id}`} className="card bg-base-100 hover:shadow-lg transition-all duration-200 
