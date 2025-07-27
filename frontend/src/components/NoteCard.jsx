15e14738ddcbbd5de0f3b2b4975fa3f77fd6826c
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2025-07-28 00:05:25 +0500

    Add delete handle

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router'
 import { formatDate } from '../lib/utils'
 
 function NoteCard({ note }) {
+
+  const handleDelete = () => {}
   return (
     <Link to={`/note/${note._id}`} className="card bg-base-100 hover:shadow-lg transition-all duration-200 
       border-t-4 border-solid border-[#00FF9D]">
