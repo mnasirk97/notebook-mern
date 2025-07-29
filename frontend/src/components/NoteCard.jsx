d739fb443f19dd6a24c6d0f693377e81ec7e97f7
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2025-07-29 23:13:22 +0500

    removed comment

--- frontend/src/components/NoteCard.jsx
+++ frontend/src/components/NoteCard.jsx
@@ -6,7 +6,7 @@ import { formatDate } from '../lib/utils'
 function NoteCard({ note }) {
 
   const handleDelete = () => {
-    {/* 2:5 */}
+    {/* 2: */}
   }
   return (
     <Link to={`/note/${note._id}`} className="card bg-base-100 hover:shadow-lg transition-all duration-200 
