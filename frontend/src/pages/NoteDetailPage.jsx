88811da6768a519dda71340baec41532fe8d03fb
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2025-08-03 22:44:27 +0500

    Add navigation and parameter handling to NoteDetailPage

--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -1,9 +1,17 @@
 import React, { useState } from 'react'
+import { useNavigate, useParams } from 'react-router'
 
 const NoteDetailPage = () => {
   const [note, setNote] = useState(null)
   const [loading, setLoading] = useState(true)
   const [saving, setSaving] = useState(false)
+
+  const navigate = useNavigate()
+
+  const {id} = useParams()
+
+  console.log("Note ID =>", {id})
+
   return (
     <div>
       NoteDetailPage
