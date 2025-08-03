aae1508a6b00087ba234d552512cb215beeb23d5
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2025-08-03 22:56:09 +0500

    Add note fetching logic and loading state to NoteDetailPage

--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -1,5 +1,8 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import { useNavigate, useParams } from 'react-router'
+import api from '../lib/axios'
+import toast from 'react-hot-toast'
+import { LoaderIcon } from 'lucide-react'
 
 const NoteDetailPage = () => {
   const [note, setNote] = useState(null)
@@ -10,7 +13,33 @@ const NoteDetailPage = () => {
 
   const {id} = useParams()
 
-  console.log("Note ID =>", {id})
+  // console.log("Note ID =>", {id})
+
+  useEffect(() => {
+    const fetchNote = async () => {
+      try {
+        const res = await api.get(`/notes/${id}`)
+        setNote(res.data.data)
+      } catch (error) {
+        toast.error('Failed to fetch note. Please try again later.')
+        console.error("Error fetching note:", error)
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchNote()
+  }, [id])
+
+  console.log("Note =>", {note})
+
+  if (loading) {
+    return (
+      <div className="min-h-screen bg-base-200 flex items-center justify-center">
+        <LoaderIcon className="animate-spin size-10" />
+      </div>
+    );
+  }
 
   return (
     <div>
