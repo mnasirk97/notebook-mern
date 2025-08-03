219cb1433abf1b5525d74863a688df08f03afa91
muhammad nasir khan <khanmuhammadnasir9977@gmail.com>
2025-08-03 22:39:51 +0500

    Add states

--- frontend/src/pages/NoteDetailPage.jsx
+++ frontend/src/pages/NoteDetailPage.jsx
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const NoteDetailPage = () => {
+  const [note, setNote] = useState(null)
+  const [loading, setLoading] = useState(true)
+  const [saving, setSaving] = useState(false)
   return (
     <div>
       NoteDetailPage
